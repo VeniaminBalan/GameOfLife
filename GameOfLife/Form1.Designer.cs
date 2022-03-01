3d9636d871482c7700f7f13524d97663780adf9a
VeniaminBalan <99888346+VeniaminBalan@users.noreply.github.com>
2022-03-01 23:04:31 +0200

    Add files via upload

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Game_of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Continue = new System.Windows.Forms.Button();
+            this.ClearStart = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudResolution = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Continue);
+            this.splitContainer1.Panel1.Controls.Add(this.ClearStart);
+            this.splitContainer1.Panel1.Controls.Add(this.Stop);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDensity);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.nudResolution);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(817, 404);
+            this.splitContainer1.SplitterDistance = 129;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Continue
+            // 
+            this.Continue.Location = new System.Drawing.Point(13, 179);
+            this.Continue.Margin = new System.Windows.Forms.Padding(2);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(62, 32);
+            this.Continue.TabIndex = 7;
+            this.Continue.Text = "Continue";
+            this.Continue.UseVisualStyleBackColor = true;
+            this.Continue.Click += new System.EventHandler(this.ContinueButton);
+            // 
+            // ClearStart
+            // 
+            this.ClearStart.Location = new System.Drawing.Point(13, 224);
+            this.ClearStart.Margin = new System.Windows.Forms.Padding(2);
+            this.ClearStart.Name = "ClearStart";
+            this.ClearStart.Size = new System.Drawing.Size(62, 32);
+            this.ClearStart.TabIndex = 6;
+            this.ClearStart.Text = "Clear";
+            this.ClearStart.UseVisualStyleBackColor = true;
+            this.ClearStart.Click += new System.EventHandler(this.bClear);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(13, 136);
+            this.Stop.Margin = new System.Windows.Forms.Padding(2);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(62, 32);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.bStops);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 96);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.bStart);
+            // 
+            // nudDensity
+            // 
+            this.nudDensity.Location = new System.Drawing.Point(2, 68);
+            this.nudDensity.Margin = new System.Windows.Forms.Padding(2);
+            this.nudDensity.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudDensity.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDensity.Name = "nudDensity";
+            this.nudDensity.Size = new System.Drawing.Size(80, 20);
+            this.nudDensity.TabIndex = 3;
+            this.nudDensity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudDensity.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(2, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Density";
+            // 
+            // nudResolution
+            // 
+            this.nudResolution.Location = new System.Drawing.Point(2, 23);
+            this.nudResolution.Margin = new System.Windows.Forms.Padding(2);
+            this.nudResolution.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudResolution.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudResolution.Name = "nudResolution";
+            this.nudResolution.Size = new System.Drawing.Size(80, 20);
+            this.nudResolution.TabIndex = 1;
+            this.nudResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudResolution.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(2, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resolution";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(681, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 404);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Game of Life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown nudResolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown nudDensity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button ClearStart;
+        private System.Windows.Forms.Button Continue;
+    }
+}
+
---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graphis;
+        private int resolution;
+        private GameEngine gameEengine;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawNextGeneration();
+        }
+
+        private void bStart(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void bStops(object sender, EventArgs e)
+        {
+            if (gameEengine == null) return;
+            StopGame();
+        }
+        private void ContinueButton(object sender, EventArgs e)
+        {
+            if (gameEengine == null) return;
+            ContinueGame();
+        }
+
+        private void bClear(object sender, EventArgs e)
+        {
+            if (gameEengine == null) return;
+            ClearButton();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (gameEengine == null) return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                gameEengine.AddCell(x, y);
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                gameEengine.RemoveCell(x, y);
+            }
+        }
+
+        private void StartGame()
+        {
+            nudDensity.Enabled = false;
+            nudResolution.Enabled = false;
+            resolution = (int)nudResolution.Value;
+
+            gameEengine = new GameEngine
+            (
+                rows: pictureBox1.Height / resolution,
+                cols: pictureBox1.Width / resolution,
+                density: (int)(nudDensity.Minimum) + (int)(nudDensity.Maximum) - (int)nudDensity.Value
+            );
+
+            Text = $"Generation : {gameEengine.currentGeneration}";
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphis = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+
+        private void DrawNextGeneration()
+        {
+            graphis.Clear(Color.Black);
+
+            var field = gameEengine.GetCurrentGeneration();
+
+            for (int x = 0; x < field.GetLength(0) ; x++)
+            {
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    if (field[x, y]) 
+                    {
+                        graphis.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                }
+            }
+
+            pictureBox1.Refresh();
+            Text = $"Generation : {gameEengine.currentGeneration}";
+            gameEengine.NextGeneration();
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            nudDensity.Enabled = true;
+            nudResolution.Enabled = true;
+        }
+
+        
+
+        private void ContinueGame()
+        {
+            if (timer1.Enabled)
+                return;
+            timer1.Start();
+            nudDensity.Enabled = false;
+            nudResolution.Enabled = false;
+        }
+
+
+
+        private void ClearButton()
+        {
+            nudDensity.Enabled = false;
+            nudResolution.Enabled = false;
+            resolution = (int)nudResolution.Value;
+
+            gameEengine.Clear
+            (
+                rows: pictureBox1.Height / resolution,
+                cols: pictureBox1.Width / resolution
+            );
+            timer1.Start();
+        }
+    }
+}
